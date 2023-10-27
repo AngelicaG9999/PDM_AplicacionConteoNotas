221d2846d8d53efbea578dacccc2b432b8aa70c8
Angelica <angelicagomez9905@gmail.com>
2023-10-27 00:01:13 -0500

    Codigo con funcionalidad requerida
    Se creo MainActivity y su dialog

--- app/src/main/java/com/example/pdm_aplicacionconteonotas/MainActivity.java
+++ app/src/main/java/com/example/pdm_aplicacionconteonotas/MainActivity.java
@@ -1,78 +1,108 @@
 package com.example.pdm_aplicacionconteonotas;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.View;
-
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.pdm_aplicacionconteonotas.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private EditText etNumeroEstudiantes;
+    private CheckBox cBmostrarConteoEstudiantesPerdieron;
+
+    private RadioButton rBMostrarNotaDefinitiva;
+    private TextView tvResultado;
+    private ArrayList<Integer> estudiantesQuePerdieron = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        etNumeroEstudiantes = findViewById(R.id.etNumeroEstudiantes);
+        cBmostrarConteoEstudiantesPerdieron = findViewById(R.id.cBmostrarConteoEstudiantesPerdieron);
+        rBMostrarNotaDefinitiva = findViewById(R.id.rBMostrarNotaDefinitiva);
+        tvResultado = findViewById(R.id.tvResultado);
+    }
 
-        setSupportActionBar(binding.toolbar);
+    public void calcularNotaDefinitiva(View view) {
+        tvResultado.setText("");
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        String numeroEstudiantesTxt = etNumeroEstudiantes.getText().toString();
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        if (!numeroEstudiantesTxt.isEmpty()) {
+            int numeroEstudiantes = Integer.parseInt(numeroEstudiantesTxt);
+
+            if (numeroEstudiantes > 0) {
+                mostrarDialogoNotas(1);
+            } else {
+                tvResultado.setText("Ingrese un número válido de estudiantes.");
+            }
+        } else {
+            tvResultado.setText("El campo de número de estudiantes está vacío.");
+        }
+    }
+
+    private void mostrarDialogoNotas(final int numeroEstudiante) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Ingrese notas del estudiante " + numeroEstudiante);
+        View view = getLayoutInflater().inflate(R.layout.dialog_notas, null);
+        final EditText etNota1Dialog = view.findViewById(R.id.etNota1Dialog);
+        final EditText etNota2Dialog = view.findViewById(R.id.etNota2Dialog);
+        final EditText etNota3Dialog = view.findViewById(R.id.etNota3Dialog);
+        final EditText etNota4Dialog = view.findViewById(R.id.etNota4Dialog);
+        builder.setView(view);
+
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+            public void onClick(DialogInterface dialog, int which) {
+                double nota1 = Double.parseDouble(etNota1Dialog.getText().toString());
+                double nota2 = Double.parseDouble(etNota2Dialog.getText().toString());
+                double nota3 = Double.parseDouble(etNota3Dialog.getText().toString());
+                double nota4 = Double.parseDouble(etNota4Dialog.getText().toString());
+
+                if (nota1 < 1 || nota1 > 5 || nota2 < 1 || nota2 > 5 || nota3 < 1 || nota3 > 5 || nota4 < 1 || nota4 > 5) {
+                    tvResultado.setText("Las notas deben estar entre 1 y 5.");
+                    return;
+                }
+
+                double notaFinal = (nota1 * 0.20) + (nota2 * 0.30) + (nota3 * 0.15) + (nota4 * 0.35);
+
+                if (rBMostrarNotaDefinitiva.isChecked()) {
+                    tvResultado.append("\nLa nota definitiva del estudiante " + numeroEstudiante + " es: " + String.format("%.2f", notaFinal));
+                }
+
+                if (notaFinal < 3.0) {
+                    tvResultado.append("\nEste estudiante perdió la materia.");
+                    estudiantesQuePerdieron.add(numeroEstudiante);
+                }
+
+                if (numeroEstudiante < Integer.parseInt(etNumeroEstudiantes.getText().toString())) {
+                    mostrarDialogoNotas(numeroEstudiante + 1);
+                } else {
+                    if ( cBmostrarConteoEstudiantesPerdieron.isChecked()) {
+                        mostrarConteoEstudiantesPerdieron();
+                    }
+                }
             }
         });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    private void mostrarConteoEstudiantesPerdieron() {
+        tvResultado.append("\nNúmero de estudiantes que perdieron: " + estudiantesQuePerdieron.size());
     }
 }
